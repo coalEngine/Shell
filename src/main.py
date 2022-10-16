32add0472bef0899d5fc2a086ed9df69e39d97b9
Thiery <weirdogaming@gmail.com>
2022-10-16 18:07:27 -0400

    Update

--- src/main.py
+++ src/main.py
@@ -3,10 +3,17 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+
+
+class oto(Widget):
+    pass
 
 class myApp(App):
     def build(self):
-        return Label(text="Shell")
+        return oto()
 
 
 if __name__ == "__main__":
